8750b92743f934e9fa80a29b95afa4519e2d7f45
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 16:43:07 +0300

    First Commit

---
+++ JSDOMExamples.js
@@ -0,0 +1,54 @@
+// Boş bir div oluşturun ve budive border radius border genişlik yükseklik arka plan rengi ve radius ekleyin
+
+// var jhon=document.getElementById("jhon")
+// jhon.style.height="500px"
+// jhon.style.width="500px"
+// jhon.style.border="7px solid darkblue"
+// jhon.style.borderRadius="5px"
+// jhon.style.backgroundColor="yellow"
+
+// Boş bir div olsun ve divin özelliklerini inputlardan alarak oluşturun yükseklik genişlik arka plan çerçeve ve ovallik
+
+// 1.YÖNTEM (BENİM YAPTIĞIM)
+// var jhon=document.getElementById("jhon")
+// var btn=document.getElementById("btn")
+// var border=document.getElementById("border")
+// var borderRadius=document.getElementById("borderRadius")
+// var backgroundColor=document.getElementById("backgroundColor")
+// var width=document.getElementById("width")
+// var height=document.getElementById("height")
+
+
+//         btn.onclick=()=>{
+//             jhon.style.height=height.value
+//             jhon.style.width=width.value
+//             jhon.style.border=border.value
+//             jhon.style.backgroundColor=backgroundColor.value
+//             jhon.style.borderRadius=borderRadius.value
+            
+//         }
+
+//2.YÖNTEM(HOCANIN YAPTIĞI)
+var jhon=document.getElementById("jhon")
+var btn=document.getElementById("btn")
+var border=document.getElementById("border")
+var borderRadius=document.getElementById("borderRadius")
+var backgroundColor=document.getElementById("backgroundColor")
+var width=document.getElementById("width")
+var height=document.getElementById("height")
+
+        btn.onclick=()=>{
+            jhon.style.height=height.value
+            jhon.style.width=width.value
+            jhon.style.border=border.value
+            jhon.style.backgroundColor=backgroundColor.value
+            jhon.style.borderRadius=borderRadius.value
+            
+        }
+        
+
+
+    
+        
+
+    
